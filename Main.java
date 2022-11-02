3c40ebc2d0c57fcd008647dd192a59185a71607b
Vincent <tran.vincent@outlook.com>
2022-11-02 02:50:02 -0400

    Removed Vector.java, refactored Vertex class in Polygon.java to Point

--- Main.java
+++ Main.java
@@ -2,80 +2,12 @@ package JavaPolygon;
 
 public class Main {
     public static void main(String[] args) {
+        Polygon polygon = new Polygon();
+        polygon.addPoint(1, 2);
+        polygon.addPoint(4, 4);
+        polygon.addPoint(-2, 3);
+        polygon.addPoint(-3, 1);
 
-        Polygon rect = new Polygon();
-        rect.addVertex(1, 2);
-        rect.addVertex(4, 4);
-        rect.addVertex(-2, 3);
-        rect.addVertex(-3, 1);
-
-        Vector[] plot = rect.vectorize();
-        printCoords(plot);
-
-        String[][] grid = plot(plot);
-        printGrid(grid);
-
-    }
-
-    public static String[][] plot(Vector[] plot) {
-        String[][] grid = makeGrid(plot);
-
-        // y = mx+b
-        // y = plot[i].getY()/plot[i].getX() * k
-        // truncate y 
-        for (int i = 0; i < plot.length; i++) {
-            int slope = (int) (plot[i].getY()/plot[i].getY());
-            for (int j = 0; j < grid[0].length; j++) {
-                int y = slope * j;
-                if (y > grid.length) {
-                    break;
-                }
-                grid[y][j] = " * ";
-            }
-        }
-
-        return grid;
-    }
-
-    public static String[][] makeGrid(Vector[] plot) {
-        double[] maxValues = getMax(plot);
-        String[][] grid = new String[(int) maxValues[0] + 1][(int) maxValues[1] + 1];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j= 0; j < grid[i].length; j++) {
-                grid[i][j] = " _ ";
-            }
-        }
-        return grid;
-    }
-
-    public static double[] getMax(Vector[] plot) {
-        double maxX = 0;
-        double maxY = 0;
-        for (int i = 0; i < plot.length; i++) {
-            double currentX = plot[i].getX();
-            double currentY = plot[i].getY();
-            if (currentX > maxX) {
-                maxX = currentX;
-            }
-            if (currentY > maxY) {
-                maxY = currentY;
-            }
-        }
-        return new double[] { maxX, maxY }; 
-    }
-
-    public static void printGrid(String[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                System.out.print(grid[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    public static void printCoords(Vector[] plot) {
-        for (int i = 0; i < plot.length; i++) {
-            System.out.println(plot[i].toString());
-        }
+        System.out.println(polygon.toString());
     }
 }
--- Polygon.java
+++ Polygon.java
@@ -1,92 +1,173 @@
 package JavaPolygon;
 
 public class Polygon {
-    Vertex head;
-    Vertex tail;
+    Point head;
+    Point tail;
     int sides;
 
+    /**
+     * Initializes a new "empty" Polygon instance 
+     */
     public Polygon() {
         head = null;
         tail = null;
         sides = 0;
     }
 
-    public void addVertex(double x, double y) {
-        Vertex tempVertex = new Vertex(x, y);
-        tempVertex.setNext(head);
-        if (head == null) {
-            tail = tempVertex;
+    /**
+     * Initializes a new Polygon instances with given points
+     * 
+     * @param coordsArr double array containing coordinate tuples
+     */
+    public Polygon(double[][] coordsArr) {
+        head = null;
+        tail = null;
+        sides = 0;
+        for (int i = 0; i < points.length; i++) {
+            this.addPoint(new Point(coordsArr[i][0], coordsArr[i][1]));
         }
-        head = tempVertex;
+    }
 
+    /**
+     * Adds a new point to Polygon
+     * 
+     * @param x Desired X coordinate of point
+     * @param y Desired Y coordinate of point
+     */
+    public void addPoint(double x, double y) {
+        Point tempPoint = new Point(x, y);
+        tempPoint.setNext(head);
+        if (head == null) {
+            tail = tempPoint;
+        }
+        head = tempPoint;
         tail.setNext(head);
         sides++;
     }
 
-    public Vector[] vectorize() throws NullPointerException {
-        Vector[] transformations = 
+    /**
+     * Adds a new point to Polygon
+     * 
+     * @param coords double[] tuple containing desired coordinates for Point
+     */
+    public void addPoint(double[] coords) {
+        Point tempPoint = new Point(coords[0], coords[1]);
+        point.setNext(head);
+        if (head == null) {
+            tail = tempPoint;
+        }
+        head = tempPoint;
+        tail.setNext(head);
+        sides++;
     }
 
-    private Vector[] getVertexTransformations () throws NullPointerException {
-        Vector[] vectorArr = new Vector[sides];
-        
-        Vertex tempVertex = head;
-        if (tempVertex == null) {
-            throw new NullPointerException();
+    public String toString() {
+        String returnString = "[ Sides: " + sides + " | Points: ";
+        Point tempPoint = head;
+        int i = 0;
+        while (i < sides) {
+            returnString += tempPoint.toString();
+            if (tempPoint != tail) {
+                returnString += ", ";
+            }
+            tempPoint = tempPoint.getNext();
+            i++;
         }
-
-        for (int i = 0; i < vectorArr.length; i++) {
-            Vector v1 = tempVertex.get();
-            Vector v2 = tempVertex.next.get();
-            v2 = v2.subtract(v1);
-            vectorArr[i] = v2;
-            tempVertex = tempVertex.next();
-        }
-
-        return vectorArr;
+        returnString += (" | Head " + head.toString() + " | Tail: " + tail.toString() + " ]");
+        return returnString;
     }
 
+    class Point {
+        private double positionX;
+        private double positionY;
+        private Point next;
     
-
-    public Vector[] normalize() {
-        Vector[] vectorArr = new Vector[sides];
-        
-        Vertex tempVertex = head;
-        if (tempVertex == null) {
-            throw new NullPointerException();
-        }
-
-        for (int i = 0; i < vectorArr.length; i++) {
-            Vector tempVector = tempVertex.get();
-            vectorArr[i] = tempVector.normalize();
-            tempVertex = tempVertex.next();
-        }
-
-        return vectorArr;
-    }
-
-    class Vertex {
-        private double coordX;
-        private double coordY;
-
-        private Vertex next;
-    
-        public Vertex(double x, double y) {
-            coordX = x;
-            coordY = y;
+        /**
+         * Initializes a new "empty" Point at position (0, 0) 
+         */
+        public Point() {
+            positionX = 0;
+            positionY = 0;
             next = null;
         }
-
-        public Vertex next() {
-            return next;
+    
+        /**
+         * Initializes a new point at (x, y)
+         * 
+         * @param x Desired X coordinate of point
+         * @param y Desired Y coordinate of point
+         */
+        public Point(double x, double y) {
+            positionX = x;
+            positionY = y;
+            next = null;
         }
     
-        public void setNext(Vertex nextVertex) {
-            next = nextVertex;
+        /**
+         * Returns next Point that this current point is pointing to
+         * 
+         * @return 
+         */
+        public Point getNext() {
+            return next;
         }
 
-        public Vector get() {
-            return new Vector(coordX, coordY);
+        /**
+         * Links current Point object to newNext
+         * 
+         * @param newNext New Point to be linked
+         */
+        public void setNext(Point newNext) {
+            next = newNext;
+        }
+
+        /**
+         * Returns double value containing X coordinate of Point
+         * 
+         * @return double containing X coordinate of Point
+         */
+        public double getX() {
+            return positionX;
+        }
+    
+        /**
+         * Returns double value containing Y coordinate of Point
+         * 
+         * @return double containing Y coordinate of Point
+         */
+        public double getY() {
+            return positionY;
+        }
+    
+        /**
+         * Sets current Point's coordinates to new coordinates
+         * 
+         * @param x Desired X coordinate to be set
+         * @param y Desired Y coordinate to be set
+         */
+        public void set(double x, double y) {
+            positionX = x;
+            positionY = y;
+        }
+    
+        public Point add(Point point) {
+            return new Point(positionX + point.getX(), positionY + point.getY());
+        }
+    
+        public Point add(double amount) {
+            return new Point(positionX + amount, positionY + amount);
+        }
+    
+        public Point subtract(Point point) {
+            return new Point(positionX - point.getX(), positionY - point.getY());
+        }
+    
+        public Point subtract(double amount) {
+            return new Point(positionX - amount, positionY - amount);
+        }
+    
+        public String toString() {
+            return "( " + positionX + ", " + positionY + " )";
         }
     }
 }
